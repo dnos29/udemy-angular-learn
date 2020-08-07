1ccc265fdde221a62776b6fdd59493b440c527e3
sondn <sondn@mog.net>
2020-08-07 18:30:16 +0700

    built-in observable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { User } from './user.model';
+import { interval, Subscription } from 'rxjs';
 
 const users: User[] = [
   {id: 1, name: "sondn"},
@@ -11,16 +12,21 @@ const users: User[] = [
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: User;
-
+  firstSubcription: Subscription
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      let id = +params.id;
-      this.user = users.find(u => u.id === id);
+    this.firstSubcription = interval(2000).subscribe(counter => {
+      console.log(counter);
+
     })
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestroy");
+    this.firstSubcription.unsubscribe()
   }
 
 }
