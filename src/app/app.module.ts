75d1dfb314b1b1cc7f69d91a139b3995fb075e6c
sondn-mog <sondn@mog.net>
2020-08-09 16:41:42 +0700

    template-driven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'; 
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  @ViewChild('f') signupForm: NgForm;
+  answer: string = "";
+  genders = ["Male", "Female"];
+  user = {
+   username: '',
+   email: '',
+   secrect: '',
+   questionAnswer: '',
+   gender: '',  
+  }
+  formSubmitted: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+    
+  // }
 
+  onSuggestName(){
+    let suggestName = "Superuser";
+    // this.signupForm.setValue({ // set value for all input have ngModel
+    //   userData: {
+    //     username: suggestName,
+    //     email: ''
+    //   },
+    //   questionAnswer: '',
+    //   secret: 'teacher',
+    //   gender: 'Female'
+    // })
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestName,
+      }
+    })
+  }
+  
+  onSubmit(){
+    this.formSubmitted = this.signupForm.submitted;
+    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secrect = this.signupForm.value.secret;
+    this.user.questionAnswer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.signupForm.reset();
+  }
 }
